d718cc9e61357f8f78363c64ddcfc0123a22f82c
Dream97 <1248310717@qq.com>
2017-07-09 22:01:20 +0800

    图片上传设置监听

--- app/src/main/java/com/cgapp/fragment/RepairFragment.java
+++ app/src/main/java/com/cgapp/fragment/RepairFragment.java
@@ -10,7 +10,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.cgapp.R;
 import com.cgapp.Util.CompressUtil;
@@ -21,15 +23,18 @@ import com.cgapp.Util.CompressUtil;
 
 public class RepairFragment extends Fragment {
     private ImageView personImage;
+    private ImageButton pushPicture;
     public static RepairFragment getInstance()
     {
         return new RepairFragment();
     }
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_repair,container,false);
+
+        //压缩图片
         personImage = (ImageView) view.findViewById(R.id.fragment_repair_person);
         Resources res = getResources();
         Bitmap bitmap = BitmapFactory.decodeResource(res,R.drawable.repair_person);//转bitmap
@@ -38,6 +43,15 @@ public class RepairFragment extends Fragment {
         //personImage.setImageBitmap(CompressUtil.compressImage(bitmap));
         //personImage.setImageResource(R.drawable.repair_person);
         personImage.setImageBitmap(bitmap1);
+
+        //上传图片
+        pushPicture = (ImageButton) view.findViewById(R.id.push_picture);
+        pushPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"上传图片",Toast.LENGTH_LONG).show();
+            }
+        });
         return view;
     }
 }
